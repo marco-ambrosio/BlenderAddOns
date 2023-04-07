d007ce0674cf5c711dae03a04558ab051da56620
Marco Ambrosio <62962632+marco-ambrosio@users.noreply.github.com>
2023-04-07 15:44:24 +0200

    add option to chose the exported model name

--- ExportGazeboModel/export_gazebo_model_addon.py
+++ ExportGazeboModel/export_gazebo_model_addon.py
@@ -78,11 +78,13 @@ class SdfTemplate:
 
 class GazeboModel:
     
-    def export(self, overwrite):
+    def export(self, export_name, overwrite):
         ''' Execute all the operations to export a model for Gazebo 11 '''
         
         self.base_path = bpy.path.abspath('//')
         self.model_name = bpy.path.basename(bpy.context.blend_data.filepath)[:-6]
+        self.export_name = bpy.path.basename(export_name)
+        
         if not overwrite:
             ''' do something to avoid overwriting an already exported model '''
             pass
@@ -96,11 +98,11 @@ class GazeboModel:
     def create_folders(self):
         ''' Create the folder structure and save paths into class variables '''
         
-        self.model_folder = os.path.join(
-            self.base_path, self.model_name)
+        self.export_folder = os.path.join(
+            self.base_path, self.export_name)
             
         self.meshes_folder = os.path.join(
-            self.model_folder, 'meshes')
+            self.export_folder, 'meshes')
             
         os.makedirs(self.meshes_folder, exist_ok=True)
         
@@ -129,6 +131,8 @@ class GazeboModel:
         for img in bpy.data.images:
             if img.source == 'FILE':
                 name = img.name.rstrip(string.digits + '.')
+                #  if texture files are packed into the blend file
+                # unpack them temporarily and pack them again
                 if img.packed_file is not None:
                     unpacked = True
                     img.unpack()
@@ -152,16 +156,16 @@ class GazeboModel:
     def create_sdf(self):
 
         with open(os.path.join(
-            self.model_folder, 'model.sdf'), 'w') as f:
+            self.export_folder, 'model.sdf'), 'w') as f:
             
             f.write(SdfTemplate().template.substitute({
                 "model_name": self.model_name,
-                "mesh_path": f"model://{self.model_name}/meshes/{self.model_name+'.obj'}",
+                "mesh_path": f"model://{self.export_name}/meshes/{self.model_name+'.obj'}",
              }))
                          
                 
         with open(os.path.join(
-            self.model_folder, 'model.config'), 'w') as f:
+            self.export_folder, 'model.config'), 'w') as f:
             
              f.write(ConfigTemplate().template.substitute({
                 "model_name": self.model_name,
@@ -175,14 +179,14 @@ class ExportGazeboModel(Operator, ExportHelper):
     bl_idname = 'export_gazebo_model.data'
     bl_label = "Export Gazebo Model"
     
-    filename_ext = '.sdf'
+    filename_ext = ''
     
     filter_glob: StringProperty(
         default='*',
         options={'HIDDEN'},
         maxlen=255,
     )
-    
+
     overwrite: BoolProperty(
         name="Overwrite Existing",
         description="Overwrite an existing model with the same name",
@@ -191,7 +195,7 @@ class ExportGazeboModel(Operator, ExportHelper):
     
     def execute(self, context):
         gazebo_model = GazeboModel()        
-        return gazebo_model.export(self.overwrite)
+        return gazebo_model.export(self.filepath, self.overwrite)
     
 
 
